4936ea6c70501dc2e300b17e441927a8f4af1a37
Dave Thomas <dave@monstergfx.com>
2013-12-23 15:44:15 -0800

    added fun script

---
+++ modules/fun.py
@@ -0,0 +1,45 @@
+# fun.py
+#
+# Fun functions
+
+import random
+import time
+
+def playSong():
+	# the list of songs
+	#
+	songs = [
+		"alpha",
+		"beta"
+	]
+
+	# choose a song from the list at random
+	# @todo the upper bound of this should be the largest index in the songs array
+	#
+	i = random.randint(0,2)
+
+	# play the song
+	# @todo play the song via shell command?
+	#
+
+def tellJoke():
+	# the list of jokes
+	#
+	jokes = [
+		[ "Why did the chicken cross the road?", "To get to the other side."]
+	]
+
+	# say the introduction
+	#
+	say("Here's a joke for you, "+childsName+".")
+
+	# tell the first part of the joke
+	#
+
+	# wait a moment
+	#
+	time.sleep(3)
+
+	# tell the answer
+	#
+
