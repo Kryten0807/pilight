4255bd6d413c056c8fdebcf9771153fa94de13e1
Dave Thomas <dthomas@starkartthenala.com>
2013-12-23 15:19:46 -0800

    added function for introduction routine

--- modules/firstrun.py
+++ modules/firstrun.py
@@ -2,6 +2,8 @@
 #
 # functions  related to the "first run" functionality
 
+import threading
+
 # is this the first run of the application?
 #
 def isFirstRun():
@@ -14,3 +16,57 @@ def setFirstRunFlag():
 	#
 	with open('storage/firstRun', 'w') as f:
 		f.write('')
+
+# go through the introduction routine
+#
+def introduction():
+	# say the opening introduction speech
+	#
+	sayIntroduction()
+
+	# wait for a button
+	#
+	b = waitForAnyButton()
+	if( not b ):
+		return
+
+	# start blinking the lights
+	# threading code from http://stackoverflow.com/questions/2846653/python-multithreading-for-dummies
+	# if this doesn't work, try http://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread-in-python
+	#
+	t = threading.Thread(target=blinkAll, args=())
+	t.start()
+
+	# say the instructions
+	#
+	sayLightInstructions()
+
+	# stop blinking the lights
+	#
+	continueBlinking = False
+
+	# wait for a button
+	#
+	b = waitForAnyButton()
+	if( not b ):
+		return
+
+	# say the button instructions
+	#
+	sayButtonInstructions()
+
+	# reset the lights
+	#
+	checkLights()
+
+	# wait for the red button
+	#
+	b = waitForRedButton()
+	if( not b ):
+		return
+
+	# play a song
+	#
+	playSong("itchy itchy goo")
+
+
