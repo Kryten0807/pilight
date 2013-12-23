4999f0b1b2dca0736f9fceeec4bd6b0389f38df0
Dave Thomas <dave@monstergfx.com>
2013-12-23 15:55:16 -0800

    uncommented main loop; replaced pseudocode "switch" statement

--- pilight.py
+++ pilight.py
@@ -19,50 +19,55 @@ exit()
 
 # start the loop
 #
-#while True:
-#	# Light the appropriate lights
-#	#
-#	checkLights()
-#
-#	# Handle the buttons
-#	#
-#	if( checkButton(redButton) or checkButton(blackButton) ):
-#
-#		# when was the last button pressed? if more than two hours ago,
-#		# then say the appropriate greeting
-#		#
-#		if( secondsSinceLastButtonPress>inactiveTime ):
-#			# is this the first run? if so, say the first run greeting and
-#			# the instructions
-#			#
-#			if( isFirstRun() ):
-#				sayIntroduction()
-#				sayInstructions()
-#				# set the first run flag
-#				#
-#				setFirstRunFlag()
-#			else:
-#				sayGreeting()
-#		else:
-#			# the child has been playing with it & has pressed a button
-#			# recently
-#			#
-#			if( checkButton(red) ):
-#				# the red button is pressed. Announce the time
-#				#
-#				sayCurrentTime()
-#			if( checkButton(black) ):
-#				# the black button is pressed. Do something fun
-#				#
-#				switch( rand() )
-#					case 1:
-#						sayWeather()
-#					case 2:
-#						sayJoke()
-#					case 3:
-#						playSong()
-#					case 4:
-#						sayNiceThing()
-#
-#	# sleep for a brief period, then loop again
-#	time.sleep(delay)
+while True:
+	# Light the appropriate lights
+	#
+	checkLights()
+
+	# Handle the buttons
+	#
+	if( checkButton(redButton) or checkButton(blackButton) ):
+
+		# when was the last button pressed? if more than two hours ago,
+		# then say the appropriate greeting
+		#
+		if( secondsSinceLastButtonPress>inactiveTime ):
+			# is this the first run? if so, say the first run greeting and
+			# the instructions
+			#
+			if( isFirstRun() ):
+				# run the introduction script
+				#
+				introduction()
+
+				# set the first run flag
+				#
+				setFirstRunFlag()
+
+			else:
+				# say the usual greeting
+				#
+				sayGreeting()
+
+		else:
+			# the child has been playing with it & has pressed a button
+			# recently
+			#
+			if( checkButton(black) ):
+				# the black button is pressed. Announce the time
+				#
+				sayCurrentTime()
+
+			if( checkButton(red) ):
+				# the red button is pressed. Do something fun
+				#
+				n = rand()
+				if( n==0 ):
+					sayWeather()
+				elif( n==1 ):
+					sayJoke()
+				elif( n==2 ):
+					playSong()
+
+	# sleep for a brief period, then loop again
+	time.sleep(delay)
