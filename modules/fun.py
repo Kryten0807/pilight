e56294e27588b6d78f44589e5ed3c6f0ad6fbbe7
Dave Thomas <dthomas@starkartthenala.com>
2013-12-24 06:53:07 -0800

    comment cleanup

--- modules/fun.py
+++ modules/fun.py
@@ -34,7 +34,7 @@ def tellJoke():
 	say("Here's a joke for you, "+childsName+".")
 
 	# tell the first part of the joke
-	# @todo
+	# @todo tell the first part of the joke
 	#
 
 	# wait a moment
@@ -42,21 +42,21 @@ def tellJoke():
 	time.sleep(3)
 
 	# tell the answer
-	# @todo
+	# @todo tell the answer to the joke
 	#
 
 
 def giveWeatherReport():
-	# get the weather giveWeatherReport
-	# @todo
+	# get the weather
+	# @todo get the weather report
 	#
 
 	# parse the result into a simple readable version
-	# @todo
+	# @todo parse the weather report
 	#
 
 	# say the weather report
 	# see http://forecast.weather.gov/MapClick.php?lat=34.20540&lon=-119.16813730000001&unit=0&lg=english&FcstType=dwml for a sample weather report as XML
-	# @todo
+	# @todo say the weather report
 	#
 
