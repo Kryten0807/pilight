b3ad71f4cc9b8768697bdbc383eae355679e956a
Dave Thomas <dave@monstergfx.com>
2013-12-24 08:51:10 -0800

    code cleanup

--- modules/constants.py
+++ modules/constants.py
@@ -2,20 +2,22 @@
 #
 # Contains the constant values for the various parts of the pilight script
 
+import datetime
+
 # the child's name
 #
 childsName = "Robert"
-
-
 
 
 # the delay for the main loop, in seconds
 #
 delay = 0.25			# 1/4 second
 
+
 # the timeout value for waits, in seconds
 #
 timeout = 10 * 60		# 10 minutes
+
 
 # GPIO pin numbers
 #
@@ -26,12 +28,10 @@ greenLight  = 23
 yellowLight = 24
 redLight    = 25
 
+
 # time values
 # These are the times at which to turn on the appropriate lights
 #
-
-import datetime
-
 greenLightOn  = datetime.time(7,0,0)
 yellowLightOn = datetime.time(6,30,0)
 redLightOn    = datetime.time(19,0,0)
@@ -40,6 +40,7 @@ redLightOn    = datetime.time(19,0,0)
 # the "inactivity" period, in seconds
 #
 inactiveTime = 2 * 3600;	# 2 hours
+
 
 # a flag used for the blinking light function to signal when it's time to stop
 #
