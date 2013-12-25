b85b612b62ac03181965da0b7a2ed241d92edc9c
Dave Thomas <dave@monstergfx.com>
2013-12-24 23:21:15 -0800

    path cleanup

--- modules/firstrun.py
+++ modules/firstrun.py
@@ -7,14 +7,14 @@ import threading
 # is this the first run of the application?
 #
 def isFirstRun():
-	return not os.path.exists('storage/firstRun')
+	return not os.path.exists(basepath+'/storage/firstRun')
 
 # set the "first run" flag
 #
 def setFirstRunFlag():
 	# write something into the file
 	#
-	with open('storage/firstRun', 'w') as f:
+	with open(basepath+'/storage/firstRun', 'w') as f:
 		f.write('')
 
 # go through the introduction routine
