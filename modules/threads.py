f7854e68e599b7290226f16ad9374c5412abb32a
Dave Thomas <dave@monstergfx.com>
2014-01-20 13:09:52 -0800

    removed speech thread

--- modules/threads.py
+++ modules/threads.py
@@ -62,30 +62,6 @@ class LightThread (threading.Thread):
 
 
 
-# A thread class for speaking a series of lines
-#
-class SpeechThread (threading.Thread):
-
-	def __init__(self, lines):
-		threading.Thread.__init__(self)
-		self.lines = lines
-		self.stopFlag = False
-
-	def run(self):
-		for l in self.lines:
-			# has the stop flag been set? if so, return
-			#
-			if( self.stopFlag ):
-				return
-
-			# say the next line
-			#
-			say(l)
-
-	def stop(self):
-		self.stopFlag = True
-
-
 
 # A thread class for playing a song
 #
