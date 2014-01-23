64e378a4b64fa51f025f537bf88a0350256ef526
Dave Thomas <dave@monstergfx.com>
2014-01-23 15:56:48 -0800

    implemented custom request handler

--- modules/httpd.py
+++ modules/httpd.py
@@ -33,7 +33,7 @@ class HTTPServerThread (threading.Thread):
 		threading.Thread.__init__(self)
 
 		self.port         = port
-		self.handlerClass = SimpleHTTPServer.SimpleHTTPRequestHandler
+		self.handlerClass = piRequestHandler
 		self.serverClass  = BaseHTTPServer.HTTPServer
 
 		protocol     = "HTTP/1.0"
@@ -53,3 +53,80 @@ class HTTPServerThread (threading.Thread):
 		print "Serving HTTP on", sa[0], "port", sa[1], "..."
 		self.httpd.serve_forever()
 
+
+
+# the pilight
+class piRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+	rgx = None
+
+	# override the handler for GET requests
+	#
+	def do_GET(self):
+		# has the regex object been compiled yet? if not, do it now
+		#
+		if self.rgx == None:
+			self.rgx = re.compile('^\/(\?speech\=(.*))?$')
+
+		# check the path
+		#
+		m = self.rgx.search(self.path)
+
+		# did we get a match? if so, parse the results
+		#
+		speech = None
+
+		if m:
+			# did we get a match to the parameters part of the URL
+			if m.group(2):
+				speech = urllib.unquote_plus(m.group(2))
+
+		# did we get a match? if so, act on it
+		#
+		if m:
+
+			self.wfile.write(
+				"""
+<html>
+	<head>
+	</head>
+
+	<body>
+		<h1>Enter the text string:</h1>
+		<form action="/" method="GET">
+			<input type="text" name="speech" placeholder="Enter something to say" />
+			<button type="submit">Say it!</button>
+		</form>
+
+				"""
+				)
+
+			if speech:
+				self.wfile.write("<h2>Saying...</h2>")
+				self.wfile.write("<pre>")
+				self.wfile.write(speech)
+				self.wfile.write("</pre>")
+
+			self.wfile.write(
+				"""
+	</body>
+</html>
+
+				"""
+				)
+
+			# speak the text string
+			#
+			say(speech)
+
+			# and return
+			#
+			return
+
+
+		# unrecognized request? pass it to the parent
+		#
+		self.wfile.write("<pre>")
+		self.wfile.write(self.path)
+		self.wfile.write("</pre>")
+		# SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
